1117cdc08e708b077e7f6d18d39e0b5bdfb98ab3
BryanM112 <estbryanmc@gmail.com>
2026-05-14 10:53:58 -0500

    navegacion

---
+++ src/app/features/students/pages/students-page/students-page.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-students-page',
+  imports: [RouterLink],
+  templateUrl: './students-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StudentsPage {
+  readonly students = signal([
+    {id:1, name: "Juan Peréz"},
+    {id:2, name: "Diego Peréz"},
+    {id:3, name: "Josué Peréz"},
+    {id:4, name: "Matías Peréz"},
+    {id:5, name: "Fernando Peréz"}
+    
+  ])
+}
