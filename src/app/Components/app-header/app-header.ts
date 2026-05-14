c6c2b5b75f06a8c07d461464e803abb16ebc06d9
BryanM112 <estbryanmc@gmail.com>
2026-05-14 09:31:54 -0500

    feat: crear componentes standalone header y hero

---
+++ src/app/Components/app-header/app-header.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './app-header.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: "./app-header.css",
+})
+export class AppHeader {
+  readonly brand = signal("PPW Angular")
+  readonly showInfo = signal(false);
+
+  readonly toggleLabel = computed(
+    ()=> 
+    (this.showInfo() 
+    ? 'ocultar info'
+    :'Mostrar info'));
+
+  changeBrand(): void {
+    // actualizar el valor de la senal brand
+    this.brand.update((valor) => valor + '!');
+  }
+
+  resetBrand(): void {
+    this.brand.set("PPW Angular");
+  }
+
+  toggleInfo(){
+    this.showInfo.update((value)=> !value);
+  }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeader } from "./Components/app-header/app-header";
+import { AppHeroComponent } from "./Components/hero/app-hero";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'application',
+  imports: [RouterOutlet, AppHeader, AppHeroComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ppw-angular');
+  isLoggedIn = false;
+  materias = ['Programaciion','Estructura de datos','DB'];
 }
