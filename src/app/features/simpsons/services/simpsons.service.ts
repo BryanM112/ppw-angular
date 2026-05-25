505caca6d18b4273169ceb2245487a444f5eae5e
BryanM112 <estbryanmc@gmail.com>
2026-05-24 21:29:14 -0500

    feat: registrar ruta simpsons/:id en app.routes

--- src/app/features/simpsons/services/simpsons.service.ts
+++ src/app/features/simpsons/services/simpsons.service.ts
@@ -10,6 +10,7 @@ import {
   timeout,
 } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 
 @Injectable({
@@ -17,7 +18,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SimpsonsService {
   private http = inject(HttpClient);
-  private readonly baseUrl = 'https://thesimpsonsapi.com/api';
+  private readonly baseUrl = environment.apiUrl;
 
  // Devuelve un Observable tipado; no hace la llamada hasta que alguien se suscribe.
   getCharacters(page: number = 1): Observable<SimpsonsResponse> {
